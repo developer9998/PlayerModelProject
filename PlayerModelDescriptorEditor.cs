abde23516ad16e2aa5717bdbeed3a65af6904885
NachoEngine <65086429+NachoEngine@users.noreply.github.com>
2023-05-31 22:46:21 -0700

    adding lip sync support

---
+++ PlayerModelDescriptorEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PlayerModelDescriptor))]
+public class PlayerModelDescriptorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        /*if(GUILayout.Button("GUI BUTTON"))
+        {
+            Debug.Log("gui button pressed");
+        }*/
+    }
+
+}
